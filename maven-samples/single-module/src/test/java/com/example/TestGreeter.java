3829014298d6e02458ecaec50a78c1789e555770
azhar <azhar.hassan@brainplow.com>
2023-02-08 04:38:19 +0500

    second

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,11 +41,3 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
-    @Test
-  public void greetShouldIncludeGreetingMessageAzhar() {
-    String someone = "World, Thanks you making me Happy";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
-}
-
